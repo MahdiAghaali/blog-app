dec8410eb660c269f947e0b7698d74e52f9bb003
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2023-01-21 18:21:33 +0330

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_152610) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_164351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "Comment", force: :cascade do |t|
-    t.integer "Authoid"
-    t.integer "Postid"
+  create_table "comments", force: :cascade do |t|
+    t.integer "authorid"
+    t.integer "postid"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["authorid"], name: "index_comments_on_authorid"
+    t.index ["postid"], name: "index_comments_on_postid"
   end
 
-  create_table "Like", force: :cascade do |t|
-    t.integer "Authoid"
-    t.integer "Postid"
+  create_table "likes", force: :cascade do |t|
+    t.integer "authorid"
+    t.integer "postid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["authorid"], name: "index_likes_on_authorid"
+    t.index ["postid"], name: "index_likes_on_postid"
   end
 
-  create_table "Post", force: :cascade do |t|
-    t.integer "Authorid"
-    t.integer "CommentsCounter"
-    t.integer "LikesCounter"
+  create_table "posts", force: :cascade do |t|
+    t.integer "authorid"
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "commentscounter"
+    t.integer "likescounter"
+    t.index ["authorid"], name: "index_posts_on_authorid"
   end
 
-  create_table "User", force: :cascade do |t|
-    t.text "Name"
-    t.text "Photo"
-    t.text "Bio"
-    t.integer "PostsCounter"
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.text "photo"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postscounter"
+    t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "comments", "posts", column: "postid"
+  add_foreign_key "comments", "users", column: "authorid"
+  add_foreign_key "likes", "users", column: "authorid"
+  add_foreign_key "posts", "likes", column: "likescounter"
+  add_foreign_key "posts", "users", column: "authorid"
 end
