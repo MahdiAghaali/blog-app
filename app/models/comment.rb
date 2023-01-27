f27e97ec76197732582b488c3b296d9aba0ddf23
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-27 10:37:26 +0330

    Catchup

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'postid'
   belongs_to :author, class_name: 'User', foreign_key: 'authorid'
+  validates :postid, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  after_save :upadate_comments
+
+  def upadate_comments
+    counter = Comment.count('postid')
+    Post.update(commentscounter: counter)
+  end
 end
