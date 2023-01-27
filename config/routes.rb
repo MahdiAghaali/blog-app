42f7296620fb2c57b192e4f2ce9e81159e270b4d
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-27 12:23:39 +0330

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i[index show]
+  resources :posts, only: %i[index show]
 end
