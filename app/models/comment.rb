e0a550c9eb0e4e405e37f74ab2b617c34f12f614
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-27 18:13:23 +0330

    Fix typo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :post, class_name: 'Post', foreign_key: 'postid'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'authorid'
-  validates :postid, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :post_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :upadate_comments
 
   def upadate_comments
-    counter = Comment.count('postid')
+    counter = Comment.count('post_id')
     Post.update(comments_counter: counter)
   end
 end
