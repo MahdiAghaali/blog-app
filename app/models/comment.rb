d1ca9741876f2dc5b0358bf2f57e539661a14bff
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-28 18:48:50 +0330

    Refactor models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,12 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
-  belongs_to :author, class_name: 'User', foreign_key: 'authorid'
-  validates :post_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  after_save :upadate_comments
 
-  def upadate_comments
-    counter = Comment.count('post_id')
-    Post.update(comments_counter: counter)
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'authorid'
-  belongs_to :posts, class_name: 'Post', foreign_key: 'authorid'
-  validates :post_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  after_save :count_like
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-  def count_like
-    Post.increment_counter(:likes_counter, :authorid)
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
