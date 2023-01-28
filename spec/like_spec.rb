a1153c103e1b1f958fc23a8913afacde4337318d
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-28 23:30:35 +0330

    Add new spec files

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
+
 RSpec.describe Like, type: :model do
-  subject { Like.create(user_id: 3, post_id: 5) }
+  subject do
+    Like.create(id: 1, post_id: 1, author_id: 1)
+  end
+
   before { subject.save }
 
-  it 'authorid not be 0' do
-    subject.post_id = 1
-    expect(subject).to_not be_nil
+  describe 'Validations' do
+    it 'is not valid without a post' do
+      subject.post_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an author' do
+      subject.author_id = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
