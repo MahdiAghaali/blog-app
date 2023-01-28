c7deb5da84426723019047e0a2e6b7f3bee68480
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-01-28 23:32:44 +0330

    Update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(id: :asc)
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @recent_posts = @user.recent_posts
   end
 end
